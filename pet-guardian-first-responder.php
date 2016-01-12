592f0b2c63fb65bc92f3fef20e4f88deb1e807d1
David Powers <david@1xrun.com>
2016-01-12 11:41:44 -0500

    editing plugin comments

--- pet-guardian-first-responder.php
+++ pet-guardian-first-responder.php
@@ -10,16 +10,16 @@ require('vendor/twilio/sdk/Services/Twilio.php');
  * that starts the plugin.
  *
  * @link              http://example.com
- * @since             1.0.0
+ * @since             0.9
  * @package           Pet_Guardian_First_Responder
  *
  * @wordpress-plugin
  * Plugin Name:       Pet Guardian First Responder
  * Plugin URI:        http://example.com/plugin-name-uri/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Version:           0.9
+ * Author:            David A. Powers / rezon8.net
+ * Author URI:        http://rezon8.net/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       pet-guardian-first-responder
