044849af396603bff795cf7d2419f000b4c1c923
David Powers <david@1xrun.com>
2016-02-03 19:46:08 -0500

    should be the correct version of the file in includes that got mu cked up

---
+++ includes/class-pet-guardian-first-responder.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * The file that defines the core plugin class
+ *
+ * A class definition that includes attributes and functions used across both the
+ * public-facing side of the site and the admin area.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Pet_Guardian_First_Responder
+ * @subpackage Pet_Guardian_First_Responder/includes
+ */
+/**
+ * The core plugin class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      1.0.0
+ * @package    Pet_Guardian_First_Responder
+ * @subpackage Pet_Guardian_First_Responder/includes
+ * @author     Your Name <email@example.com>
+ */
+class Pet_Guardian_First_Responder {
+	/**
+	 * The loader that's responsible for maintaining and registering all hooks that power
+	 * the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      Pet_Guardian_First_Responder_Loader    $loader    Maintains and registers all hooks for the plugin.
+	 */
+	protected $loader;
+	/**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+	 */
+	protected $plugin_name;
+	/**
+	 * The current version of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $version    The current version of the plugin.
+	 */
+	protected $version;
+	/**
+	 * Define the core functionality of the plugin.
+	 *
+	 * Set the plugin name and the plugin version that can be used throughout the plugin.
+	 * Load the dependencies, define the locale, and set the hooks for the admin area and
+	 * the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct() {
+		$this->plugin_name = 'pet-guardian-first-responder';
+		$this->version = '1.0.0';
+		$this->load_dependencies();
+		$this->set_locale();
+		$this->define_admin_hooks();
+		$this->define_public_hooks();
+	}
+	/**
+	 * Load the required dependencies for this plugin.
+	 *
+	 * Include the following files that make up the plugin:
+	 *
+	 * - Pet_Guardian_First_Responder_Loader. Orchestrates the hooks of the plugin.
+	 * - Pet_Guardian_First_Responder_i18n. Defines internationalization functionality.
+	 * - Pet_Guardian_First_Responder_Admin. Defines all hooks for the admin area.
+	 * - Pet_Guardian_First_Responder_Public. Defines all hooks for the public side of the site.
+	 *
+	 * Create an instance of the loader which will be used to register the hooks
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function load_dependencies() {
+		/**
+		 * The class responsible for orchestrating the actions and filters of the
+		 * core plugin.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-pet-guardian-first-responder-loader.php';
+		/**
+		 * The class responsible for defining internationalization functionality
+		 * of the plugin.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-pet-guardian-first-responder-i18n.php';
+		/**
+		 * The class responsible for defining all actions that occur in the admin area.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-pet-guardian-first-responder-admin.php';
+		/**
+		 * The class responsible for defining all actions that occur in the public-facing
+		 * side of the site.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-pet-guardian-first-responder-public.php';
+		$this->loader = new Pet_Guardian_First_Responder_Loader();
+	}
+	/**
+	 * Define the locale for this plugin for internationalization.
+	 *
+	 * Uses the Plugin_Name_i18n class in order to set the domain and to register the hook
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function set_locale() {
+		$plugin_i18n = new Pet_Guardian_First_Responder_i18n();
+		$plugin_i18n->set_domain( $this->get_plugin_name() );
+		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
+	}
+	/**
+	 * Register all of the hooks related to the admin area functionality
+	 * of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_admin_hooks() {
+		$plugin_admin = new Pet_Guardian_First_Responder_Admin( $this->get_plugin_name(), $this->get_version() );
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+	}
+	/**
+	 * Register all of the hooks related to the public-facing functionality
+	 * of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_public_hooks() {
+		$plugin_public = new Pet_Guardian_First_Responder_Public( $this->get_plugin_name(), $this->get_version() );
+		$dummyFormId = '69';
+		$phoneFormId = '68';
+		$firstRespFormId = '62';
+		$firstRespString = "gform_pre_submission_{$firstRespFormId}";
+		//$filterStr = "gform_confirmation_{$firstRespFormId}";
+		$this->loader->add_action( $firstRespString, $plugin_public, 'filterGform' );
+		//$this->loader->add_filter( $filterStr, $plugin_public, 'filterConfirmation', 10, 3 );
+		
+		$phoneStr = "gform_pre_submission_{$dummyFormId}";
+		$this->loader->add_action( $phoneStr, $plugin_public, 'testPet' );
+		add_action( 'rest_api_init', function () {
+		    register_rest_route( 'petguardian/v1', '/twilio-response', array(
+		        'methods' => 'POST',
+		        'callback' => array('PhoneNumber','smsCallback')
+		    ) );
+		} );
+		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+	}
+	/**
+	 * Run the loader to execute all of the hooks with WordPress.
+	 *
+	 * @since    1.0.0
+	 */
+	public function run() {
+		$this->loader->run();
+	}
+	/**
+	 * The name of the plugin used to uniquely identify it within the context of
+	 * WordPress and to define internationalization functionality.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The name of the plugin.
+	 */
+	public function get_plugin_name() {
+		return $this->plugin_name;
+	}
+	/**
+	 * The reference to the class that orchestrates the hooks with the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    Plugin_Name_Loader    Orchestrates the hooks of the plugin.
+	 */
+	public function get_loader() {
+		return $this->loader;
+	}
+	/**
+	 * Retrieve the version number of the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The version number of the plugin.
+	 */
+	public function get_version() {
+		return $this->version;
+	}
+}
